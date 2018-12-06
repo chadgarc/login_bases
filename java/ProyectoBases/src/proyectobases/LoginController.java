274190896d5276757f14d4752af22ae1922dd870
Cesar <cesarcarlier@hotmail.com>
2018-12-05 23:00:07 -0500

    Creando el fxml del login
    creado el fxml del login, aun falta el codigo para cambiar al dashboard y por ahora poner usuarios dummies, probz admin/admin o user/pass

---
+++ java/ProyectoBases/src/proyectobases/LoginController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectobases;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Cesar
+ */
+public class LoginController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private TextField textUsuario;
+    @FXML
+    private PasswordField textClave;
+    @FXML
+    private Button botonLogin;
+    @FXML
+    private Label labelEstado;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
